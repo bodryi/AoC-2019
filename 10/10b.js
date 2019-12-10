bc03b4814d483ea6485446566e8805c7d4eac858
bodryi <vladmolodez@gmail.com>
2019-12-10 16:51:31 +0300

    day 10

---
+++ 10/10b.js
@@ -0,0 +1,236 @@
+const input =
+  ".#....#.###.........#..##.###.#.....##...\n" +
+  "...........##.......#.#...#...#..#....#..\n" +
+  "...#....##..##.......#..........###..#...\n" +
+  "....#....####......#..#.#........#.......\n" +
+  "...............##..#....#...##..#...#..#.\n" +
+  "..#....#....#..#.....#.#......#..#...#...\n" +
+  ".....#.#....#.#...##.........#...#.......\n" +
+  "#...##.#.#...#.......#....#........#.....\n" +
+  "....##........#....#..........#.......#..\n" +
+  "..##..........##.....#....#.........#....\n" +
+  "...#..##......#..#.#.#...#...............\n" +
+  "..#.##.........#...#.#.....#........#....\n" +
+  "#.#.#.#......#.#...##...#.........##....#\n" +
+  ".#....#..#.....#.#......##.##...#.......#\n" +
+  "..#..##.....#..#.........#...##.....#..#.\n" +
+  "##.#...#.#.#.#.#.#.........#..#...#.##...\n" +
+  ".#.....#......##..#.#..#....#....#####...\n" +
+  "........#...##...#.....#.......#....#.#.#\n" +
+  "#......#..#..#.#.#....##..#......###.....\n" +
+  "............#..#.#.#....#.....##..#......\n" +
+  "...#.#.....#..#.......#..#.#............#\n" +
+  ".#.#.....#..##.....#..#..............#...\n" +
+  ".#.#....##.....#......##..#...#......#...\n" +
+  ".......#..........#.###....#.#...##.#....\n" +
+  ".....##.#..#.....#.#.#......#...##..#.#..\n" +
+  ".#....#...#.#.#.......##.#.........#.#...\n" +
+  "##.........#............#.#......#....#..\n" +
+  ".#......#.............#.#......#.........\n" +
+  ".......#...##........#...##......#....#..\n" +
+  "#..#.....#.#...##.#.#......##...#.#..#...\n" +
+  "#....##...#.#........#..........##.......\n" +
+  "..#.#.....#.....###.#..#.........#......#\n" +
+  "......##.#...#.#..#..#.##..............#.\n" +
+  ".......##.#..#.#.............#..#.#......\n" +
+  "...#....##.##..#..#..#.....#...##.#......\n" +
+  "#....#..#.#....#...###...#.#.......#.....\n" +
+  ".#..#...#......##.#..#..#........#....#..\n" +
+  "..#.##.#...#......###.....#.#........##..\n" +
+  "#.##.###.........#...##.....#..#....#.#..\n" +
+  "..........#...#..##..#..##....#.........#\n" +
+  "..#..#....###..........##..#...#...#..#..";
+
+const ASTEROID = "#";
+
+function extractAsteroidsCoordinates(map) {
+  const asteroidsCoords = [];
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      if (map[i][j] === ASTEROID) {
+        asteroidsCoords.push({ x: j, y: i });
+      }
+    }
+  }
+
+  return asteroidsCoords;
+}
+
+function isPointBelongsToSection(sectionStart, sectionEnd, point) {
+  if (
+    Math.min(sectionStart.x, sectionEnd.x) <= point.x &&
+    point.x <= Math.max(sectionStart.x, sectionEnd.x) &&
+    Math.min(sectionStart.y, sectionEnd.y) <= point.y &&
+    point.y <= Math.max(sectionStart.y, sectionEnd.y)
+  ) {
+    if (
+      (sectionStart.x === sectionEnd.x && sectionEnd.x === point.x) ||
+      (sectionStart.y === sectionEnd.y && sectionEnd.y === point.y)
+    ) {
+      return true;
+    }
+
+    const lhs = (point.x - sectionStart.x) / (sectionEnd.x - sectionStart.x);
+    const rhs = (point.y - sectionStart.y) / (sectionEnd.y - sectionStart.y);
+
+    return lhs === rhs;
+  }
+
+  return false;
+}
+
+function getVisibleAsteroidsCountMap(asteroids) {
+  const asteroidsCountMap = new Array(asteroids.length).fill(0);
+  for (
+    let sectionStartIndex = 0;
+    sectionStartIndex < asteroids.length;
+    sectionStartIndex++
+  ) {
+    for (
+      let sectionEndIndex = 0;
+      sectionEndIndex < asteroids.length;
+      sectionEndIndex++
+    ) {
+      if (sectionStartIndex === sectionEndIndex) {
+        continue;
+      }
+      let isViewBlocked = false;
+      for (
+        let otherPointIndex = 0;
+        otherPointIndex < asteroids.length;
+        otherPointIndex++
+      ) {
+        let res =
+          otherPointIndex !== sectionStartIndex &&
+          otherPointIndex !== sectionEndIndex
+            ? isPointBelongsToSection(
+                asteroids[sectionStartIndex],
+                asteroids[sectionEndIndex],
+                asteroids[otherPointIndex]
+              )
+            : false;
+        if (res) {
+          isViewBlocked = true;
+          break;
+        }
+      }
+
+      if (!isViewBlocked) {
+        asteroidsCountMap[sectionStartIndex]++;
+      }
+    }
+  }
+
+  return asteroidsCountMap;
+}
+
+function getSurroundingAsteroids(searchPoint, asteroids) {
+  const surroundingAsteroids = [];
+  for (let i = 0; i < asteroids.length; i++) {
+    let isViewBlocked = false;
+    for (let j = 0; j < asteroids.length; j++) {
+      let res =
+        i !== j
+          ? isPointBelongsToSection(searchPoint, asteroids[i], asteroids[j])
+          : false;
+      if (res) {
+        isViewBlocked = true;
+        break;
+      }
+    }
+
+    if (!isViewBlocked) {
+      surroundingAsteroids.push({
+        ...asteroids[i]
+      });
+    }
+  }
+
+  return surroundingAsteroids;
+}
+
+function getK(point1, point2) {
+  return !!(point1.x - point2.x)
+    ? (point1.y - point2.y) / (point1.x - point2.x)
+    : "None";
+}
+
+function getSortedFourthQuarter(quarter, sortFn) {
+  const nans = quarter.filter(v => isNaN(v.angle));
+  const notNans = quarter.filter(v => !isNaN(v.angle));
+  return [...nans.sort((a, b) => b.y - a.y), ...notNans.sort(sortFn)];
+}
+
+function destroyAsteroidsSpin(asteroids, stationPoint) {
+  const destroyedAsteroids = getSurroundingAsteroids(
+    stationPoint,
+    sortedAsteroids
+  );
+  return {
+    survivedAsteroids: asteroids.filter(
+      a => !destroyedAsteroids.find(d => d.x === a.x && a.y === d.y)
+    ),
+    destroyedAsteroids
+  };
+}
+
+const data = input.split("\n").map(line => line.split(""));
+const asteroids = extractAsteroidsCoordinates(data);
+
+const asteroidsCountMap = getVisibleAsteroidsCountMap(asteroids);
+const maxVisible = asteroidsCountMap.reduce(
+  (max, curr) => (curr > max ? curr : max),
+  0
+);
+const stationIndex = asteroidsCountMap.findIndex(v => v === maxVisible);
+const stationPoint = asteroids[stationIndex];
+
+asteroids.splice(stationIndex, 1);
+
+const mapFn = v => ({
+  ...v,
+  angle: Math.atan(getK(stationPoint, v))
+});
+let firstQuarter = asteroids
+  .filter(
+    asteroid => stationPoint.x <= asteroid.x && stationPoint.y < asteroid.y
+  )
+  .map(mapFn);
+let secondQuarter = asteroids
+  .filter(
+    asteroid => stationPoint.x > asteroid.x && stationPoint.y < asteroid.y
+  )
+  .map(mapFn);
+let thirdQuarter = asteroids
+  .filter(
+    asteroid => stationPoint.x > asteroid.x && stationPoint.y >= asteroid.y
+  )
+  .map(mapFn);
+let fourthQuarter = asteroids
+  .filter(
+    asteroid => stationPoint.x <= asteroid.x && stationPoint.y >= asteroid.y
+  )
+  .map(mapFn);
+
+const sortFn = (a, b) =>
+  isNaN(a.angle) || isNaN(b.angle) ? -1 : a.angle - b.angle;
+
+firstQuarter.sort(sortFn);
+secondQuarter.sort(sortFn);
+thirdQuarter.sort(sortFn);
+fourthQuarter = getSortedFourthQuarter(fourthQuarter, sortFn);
+let sortedAsteroids = [
+  ...fourthQuarter,
+  ...firstQuarter,
+  ...secondQuarter,
+  ...thirdQuarter
+];
+
+let destroyedAsteroids = [];
+while (sortedAsteroids.length) {
+  const res = destroyAsteroidsSpin(sortedAsteroids, stationPoint);
+  sortedAsteroids = [...res.survivedAsteroids];
+  destroyedAsteroids = [...destroyedAsteroids, ...res.destroyedAsteroids];
+}
+
+console.log(destroyedAsteroids[199]);
