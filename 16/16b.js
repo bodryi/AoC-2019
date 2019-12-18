54593207c0eb4550cfca2e11e456b5f6c22e5b1a
bodryi <vladmolodez@gmail.com>
2019-12-18 14:12:34 +0300

    day 16 done

---
+++ 16/16b.js
@@ -0,0 +1,41 @@
+/**
+ *
+ * Offset is beyond half of input data, so for every element after offset pattern values will be equal 1
+ */
+
+const BOUND = 100;
+const REPEAT_TIME = 10000;
+
+function phase(input, patternBase, offset) {
+  let value = 0;
+  for (let i = input.length - 1; i >= offset; i--) {
+    value = (value + input[i]) % 10;
+    input[i] = value;
+  }
+  return input;
+}
+
+function getRepeatedInput(input, repeatTimes) {
+  const res = new Array(input.length * repeatTimes);
+  for (let i = 0; i < repeatTimes; i++) {
+    for (let j = 0; j < input.length; j++) {
+      res[i * input.length + j] = input[j];
+    }
+  }
+  return res;
+}
+
+const input = "59766977873078199970107568349014384917072096886862753001181795467415574411535593439580118271423936468093569795214812464528265609129756216554981001419093454383882560114421882354033176205096303121974045739484366182044891267778931831792562035297585485658843180220796069147506364472390622739583789825303426921751073753670825259141712329027078263584903642919122991531729298497467435911779410970734568708255590755424253797639255236759229935298472380039602200033415155467240682533288468148414065641667678718893872482168857631352275667414965503393341925955626006552556064728352731985387163635634298416016700583512112158756656289482437803808487304460165855189"
+  .split("")
+  .map(n => +n);
+const pattern = [0, 1, 0, -1];
+const offset = +input.slice(0, 7).join("");
+
+const repeatedInput = getRepeatedInput(input, REPEAT_TIME);
+let res = [...repeatedInput];
+for (let i = 0; i < BOUND; i++) {
+  res = phase([...res], pattern, offset);
+}
+
+console.log(res.slice(offset, offset + 8).join(""));
+//36265589
